50f29ec3bdef9853e73a8ef456b4ce404b1ca373
AlejandrVergara <josea.vergarar@ecci.edu.co>
2023-11-21 20:46:18 -0500

    i hate this class

--- R/Final.R
+++ R/Final.R
@@ -119,6 +119,8 @@ model <- train(sample_type ~ .,
 # Summarize the results of linear regression model
 print(model)
 
+##arboles de decision
+
 fit <- rpart(sample_type ~ .,
              method = "anova",
              data = final_data_filtradoe[, c(Predictores, "sample_type")],
@@ -150,37 +152,37 @@ print(head(output))
 ################################################
 
 
-# Convierte la variable de respuesta a factor si no lo está
+
 final_data_filtradoe$sample_type <- as.factor(final_data_filtradoe$sample_type)
 
-# Divide los datos en conjuntos de entrenamiento y prueba
+
 set.seed(123)
 sample.index <- sample(1:nrow(final_data_filtradoe), nrow(final_data_filtradoe) * 0.7, replace = FALSE)
 train.data <- final_data_filtradoe[sample.index, c(Predictores, "sample_type"), drop = FALSE]
 test.data <- final_data_filtradoe[-sample.index, c(Predictores, "sample_type"), drop = FALSE]
 
-# Realiza la búsqueda de hiperparámetros con e1071
+
 tune.out <- tune(svm,
                  sample_type ~ .,
                  data = train.data,
                  kernel = "linear",
                  ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
 
-# Extrae el mejor modelo
+
 bestmod <- tune.out$best.model
 
-# Configura el modelo SVM con un kernel lineal utilizando los mejores hiperparámetros
+
 svm_model <- svm(sample_type ~ ., data = train.data, kernel = "linear", cost = bestmod[["cost"]])
 
-# Realiza predicciones en el conjunto de prueba
+
 svm_predict <- predict(svm_model, newdata = test.data)
 
-# Evalúa el rendimiento del modelo
+
 
 confusionMatrix(data = svm_predict, reference = test.data$sample_type)
 
+plot(svm_model,data= test.data)
 
-# Realiza la búsqueda de hiperparámetros con e1071
 tune.out <- tune(svm,
                  sample_type ~ .,
                  data = train.data,
@@ -190,11 +192,10 @@ tune.out <- tune(svm,
 bestmod <- tune.out$best.model
 
 svm_model <- svm(sample_type ~ ., data = train.data, kernel = "radial", cost = bestmod[["cost"]])
-# Realiza predicciones en el conjunto de prueba
+
 svm_predict <- predict(svm_model, newdata = test.data)
 
-# Evalúa el rendimiento del modelo
-library(caret)
+
 confusionMatrix(data = svm_predict, reference = test.data$sample_type)
 
 # Realiza la búsqueda de hiperparámetros con e1071
@@ -274,3 +275,127 @@ knnPredict <- predict(knnFit, newdata = test.data)
 
 # Create the confusion matrix for k-NN
 confusionMatrix(data = knnPredict, reference = test.data$sample_type)
+
+
+# Linear regression
+
+final_data_filtradoe2 <- final_data_filtradoe2  %>%
+  mutate(sample_type = ifelse(sample_type == "Solid Tissue Normal", 1, 0))
+
+train.data <- final_data_filtradoe2[sample.index, c(Predictores, "sample_type"), drop = FALSE]
+test.data <- final_data_filtradoe2[-sample.index, c(Predictores, "sample_type"), drop = FALSE]
+
+# Fit linear regression model
+ins_model <- lm(sample_type ~ ., data = train.data)
+
+# Summary of linear regression model
+summary(ins_model)
+
+# Train the linear regression model
+train.control <- trainControl(method = "cv", number = 10)
+model <- train(sample_type ~ .,
+               data = train.data,
+               method = "lm",
+               trControl = train.control)
+
+# Summarize the results of linear regression model
+print(model)
+
+
+
+
+
+##arboles de decision
+
+fit <- rpart(sample_type ~ .,
+             method = "anova",
+             data = final_data_filtradoe2[, c(Predictores, "sample_type")],
+             control = rpart.control(xval = 10))
+
+# Print the decision tree
+print(fit)
+
+# Plot the decision tree
+rpart.plot::rpart.plot(fit)
+
+###### Bosques aleatorios
+
+fit.rf <- randomForest(sample_type ~ .,
+                       data = final_data_filtradoe2[, c(Predictores, "sample_type")])
+prediction.rf <- predict(fit.rf, test.data)
+table(test.data$sample_type, prediction.rf)
+
+
+fit.rf <- randomForest(sample_type ~ .,
+                       data = final_data_filtradoe2[, c(Predictores, "sample_type")])
+
+
+prediction.rf <- predict(fit.rf, test.data)
+output <- data.frame(Actual = test.data$sample_type, Predicted = prediction.rf)
+RMSE = sqrt(sum((output$Actual - output$Predicted)^2) / nrow(output))
+
+print(head(output))
+
+#######################################
+
+
+final_data_filtradoe2$sample_type <- as.factor(final_data_filtradoe2$sample_type)
+
+
+set.seed(123)
+sample.index <- sample(1:nrow(final_data_filtradoe2), nrow(final_data_filtradoe2) * 0.7, replace = FALSE)
+train.data <- final_data_filtradoe2[sample.index, c(Predictores, "sample_type"), drop = FALSE]
+test.data <- final_data_filtradoe2[-sample.index, c(Predictores, "sample_type"), drop = FALSE]
+
+
+tune.out <- tune(svm,
+                 sample_type ~ .,
+                 data = train.data,
+                 kernel = "linear",
+                 ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
+
+
+bestmod <- tune.out$best.model
+
+
+svm_model <- svm(sample_type ~ ., data = train.data, kernel = "linear", cost = bestmod[["cost"]])
+
+
+svm_predict <- predict(svm_model, newdata = test.data)
+
+
+
+confusionMatrix(data = svm_predict, reference = test.data$sample_type)
+
+plot(svm_model,data= test.data)
+
+tune.out <- tune(svm,
+                 sample_type ~ .,
+                 data = train.data,
+                 kernel = "radial",
+                 ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
+
+bestmod <- tune.out$best.model
+
+svm_model <- svm(sample_type ~ ., data = train.data, kernel = "radial", cost = bestmod[["cost"]])
+
+svm_predict <- predict(svm_model, newdata = test.data)
+
+
+confusionMatrix(data = svm_predict, reference = test.data$sample_type)
+
+# Realiza la búsqueda de hiperparámetros con e1071
+tune.out <- tune(svm,
+                 sample_type ~ .,
+                 data = train.data,
+                 kernel = "sigmoid",
+                 ranges = list(cost = c(0.001, 0.01, 0.1, 1, 5, 10, 100)))
+
+bestmod <- tune.out$best.model
+
+svm_model <- svm(sample_type ~ ., data = train.data, kernel = "sigmoid", cost = bestmod[["cost"]])
+# Realiza predicciones en el conjunto de prueba
+svm_predict <- predict(svm_model, newdata = test.data)
+
+# Evalúa el rendimiento del modelo
+confusionMatrix(data = svm_predict, reference = test.data$sample_type)
